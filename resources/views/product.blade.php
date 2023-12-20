925927f10bc67b3c9f7e50285c19381345f6eb2c
Alexandr Krylov <alexandr.krylov@gmail.com>
2023-12-20 13:50:44 +0600

    form add prodct on modal window

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -8,6 +8,7 @@
               rel="stylesheet"
               integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
               crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     </head>
     <body class="container-fluid">
         <div class="row flex-nowrap">
@@ -66,79 +67,62 @@
                     <div id="control" class="col-1">
                         <button type="button" class="btn btn-primary"
                                 data-bs-toggle="modal"
-                                data-bs-target="#addProdctModal">Добавить</button>
+                                data-bs-target="#addProductModal">Добавить</button>
                     </div>
-                </div>
+                </div>              
                 <div class="modal fade" id="addProductModal" tabindex="-1"
                      aria-labelledby="addProductModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h3>Добавить продукт</h3>
-                            </div>    
-                            <form method="post" action="{{ route('addProduct') }}">
-                                @csrf
-
-                                <div>
-                                    <label>Артикул<input type="text" name="article"></label>
-                                </div>
-                                <div>
-                                    <lable>Название<input type="text" name="name"></lable>
-                                </div>
-                                <div>
-                                    <lable>Статус
-                                        <select name="status">
-                                            <option value="available">Доступен</option>
-                                            <option value="unavailable">Не доступен</option>
-                                        </select>
-                                    </lable>
-                                </div>
-                                <h4>Атрибуты</h4>
-                                <div>
-                                    <div>
-                                        <label>Название<input type="text" name="title[]">
-                                        </label>
-                                        <label>Значение<input type="text" name="value[]">
-                                        </label>
+                                <h3 class="modal-title fs-5" id="addProductModalLabel">Добавить продукт</h3>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form method="post" action="{{ route('addProduct') }}">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <lable class="form-label">Артикул<input type="text" name="article" class="form-control"></lable>
                                     </div>
-                                    <div>
-                                        <label>Название<input type="text" name="title[]">
-                                        </label>
-                                        <label>Значение<input type="text" name="value[]">
-                                        </label>
+                                    <div class="mb-3">
+                                        <lable class="form-label">Название<input type="text" name="name" class="form-control"></lable>
                                     </div>
-                                </div>
-                                <button type="submit" name="button">Добавить</button>
-                            </form>
+                                    <div class="mb-3">
+                                        <lable class="form-label">Статус
+                                            <select name="status" class="form-select">
+                                                <option value="available">Доступен</option>
+                                                <option value="unavailable">Не доступен</option>
+                                            </select>
+                                        </lable>
+                                    </div>
+                                    <h4>Атрибуты</h4>
+                                    <div id="attributes">
+                                        <div class="row">
+                                            <div class="col">
+                                                <label class="form-label">Название<input type="text" name="title[]" class="form-control">
+                                                </label>
+                                            </div>
+                                            <div class="col">
+                                                <label class="form-label">Значение<input type="text" name="value[]" class="form-control">
+                                                </label>
+                                            </div>
+                                            <div class="col position-relative">
+                                                <button type="button" class="btn position-absolute bottom-0"><i class="bi bi-trash"></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button type="button" id="addAttribute" class="btn">+Добавить атрибут</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
             </div>
         </div>
-        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
