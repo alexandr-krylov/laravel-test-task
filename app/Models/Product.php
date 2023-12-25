b320c8b3650ae7450082a8054b0f7741187daffa
Alexandr Krylov <alexandr.krylov@gmail.com>
2023-12-25 14:15:46 +0600

    switch on soft deleting

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     const IS_AVAILABLE = 'available';
 
